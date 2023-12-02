2a4d1c1db1fe54a556cbbdd9fcfb83785951a453
tomoya sunda <dexn.dev@outlook.jp>
2023-12-02 15:21:23 +0900

    Remove r01

--- spec/factories/posts.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :post do
-    association :user
-
-    content { Faker::Lorem.paragraph }
-    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/files/test_image.jpg')) }
-  end
-end
