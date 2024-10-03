19e6a79558ab92b687be49c89d6f9eb97a036ef7
sunda <8mo10dance@protonmail.com>
2024-10-03 18:52:07 +0900

    mv r01 lo/rails

--- app/blueprints/post_blueprint.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class PostBlueprint < Blueprinter::Base
-  identifier :id
-
-  fields :content
-end
--- app/controllers/api/v1/posts_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::PostsController < ApplicationController
-  def index
-    @posts = Post.all
-    render json: { posts: PostBlueprint.render(@posts) }
-  end
-end
