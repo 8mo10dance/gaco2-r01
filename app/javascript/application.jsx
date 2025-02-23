1aeed7903107fa0ea9ce40ee849e07ff9932533d
sunda <8mo10dance@protonmail.com>
2025-02-23 14:47:33 +0900

    Use React

---
+++ app/javascript/application.jsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+
+const App = () => (
+  <>
+    <h1>Home#show</h1>
+    <p>Find me in app/views/home/show.html.erb</p>
+  </>
+);
+
+document.addEventListener("DOMContentLoaded", () => {
+  const domNode = document.getElementById("root");
+  if (domNode === undefined) return;
+
+  const root = createRoot(domNode);
+  root.render(<App />);
+});
